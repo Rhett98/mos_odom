5ea87a4afdae2227e7cf7f39d21712efde9bfc6e
yu <373914367@qq.com>
2023-05-08 16:12:33 +0800

    motion test

---
+++ infer_motion.py
@@ -0,0 +1,263 @@
+import os
+import sys
+path = os.getcwd()
+sys.path.append(path)
+import time
+import datetime
+import argparse
+import yaml
+import numpy as np
+from tqdm import tqdm
+import shutil
+
+import torch
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+
+from utility.KNN import KNN
+from utility.dataset.kitti.parser_multiscan import Parser
+from modules.model_infer import MotionNet
+from utility.warmupLR import *
+from utility.geometry import get_transformation_matrix_quaternion
+from utility.dataset.kitti.utils import write_poses, load_calib
+
+def load_model(weight_path, model):
+    state_dict = model.state_dict()
+
+    ckpt = torch.load(weight_path)
+    pretrained_dict = ckpt["state_dict"]
+
+    for key in state_dict:
+        if key in pretrained_dict:
+            state_dict[key] = pretrained_dict[key]
+        else:
+            print(key)
+
+    model.load_state_dict(state_dict, strict=True)
+    return model
+
+class User():
+  def __init__(self, ARCH, DATA, datadir, logdir, modeldir, split):
+    # parameters
+    self.ARCH = ARCH
+    self.DATA = DATA
+    self.datadir = datadir
+    self.logdir = logdir
+    self.modeldir = modeldir
+    self.split = split
+
+    # get the data
+    self.parser = Parser(root=self.datadir,
+                    train_sequences=DATA["split"]["train"],
+                    valid_sequences=DATA["split"]["valid"],
+                    test_sequences=self.DATA["split"]["test"],
+                    split=self.split,
+                    labels=DATA["labels"],
+                    color_map=DATA["color_map"],
+                    learning_map=DATA["learning_map"],
+                    learning_map_inv=DATA["learning_map_inv"],
+                    sensor=ARCH["dataset"]["sensor"],
+                    max_points=ARCH["dataset"]["max_points"],
+                    batch_size=ARCH["train"]["batch_size"],
+                    workers=ARCH["train"]["workers"],
+                    gt=True,
+                    shuffle_train=True)
+    
+    calib_file = os.path.join(self.datadir, "sequences",
+                            "08",  "calib.txt")
+    T_cam_velo = load_calib(calib_file)
+    self.T_cam_velo = np.asarray(T_cam_velo).reshape((4, 4))
+    self.T_velo_cam = np.linalg.inv(T_cam_velo)
+
+    with torch.no_grad():
+        self.model = MotionNet(input_scan=ARCH["train"]["n_input_scans"],
+                       motion_backbone=ARCH["train"]["motion_backbone"])
+        # w_dict = torch.load(modeldir, map_location=lambda storage, loc: storage)
+        self.model = load_model(modeldir, self.model)
+        # self.model.load_state_dict(w_dict['state_dict'], strict=True)
+
+    # GPU?
+    self.gpu = False
+    self.model_single = self.model
+    self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Infering in device: ", self.device)
+    if torch.cuda.is_available() and torch.cuda.device_count() > 0:
+      cudnn.benchmark = True
+      cudnn.fastest = True
+      self.gpu = True
+      self.model.cuda()
+
+  def infer(self):
+    cnn = []
+    knn = []
+    if self.split == None:
+
+        self.infer_subset(loader=self.parser.get_train_set(),
+                          to_orig_fn=self.parser.to_original, cnn=cnn, knn=knn)
+
+        # do valid set
+        self.infer_subset(loader=self.parser.get_valid_set(),
+                          to_orig_fn=self.parser.to_original, cnn=cnn, knn=knn)
+        # do test set
+        self.infer_subset(loader=self.parser.get_test_set(),
+                          to_orig_fn=self.parser.to_original, cnn=cnn, knn=knn)
+
+
+    elif self.split == 'valid':
+        self.infer_subset(loader=self.parser.get_valid_set(),
+                        to_orig_fn=self.parser.to_original, cnn=cnn, knn=knn)
+    elif self.split == 'train':
+        self.infer_subset(loader=self.parser.get_train_set(),
+                        to_orig_fn=self.parser.to_original, cnn=cnn, knn=knn)
+    else:
+        self.infer_subset(loader=self.parser.get_test_set(),
+                        to_orig_fn=self.parser.to_original, cnn=cnn, knn=knn)
+    print("Mean CNN inference time:{}\t std:{}".format(np.mean(cnn), np.std(cnn)))
+    print("Mean KNN inference time:{}\t std:{}".format(np.mean(knn), np.std(knn)))
+    print("Total Frames:{}".format(len(cnn)))
+    print("Finished Infering")
+
+    return
+
+  def infer_subset(self, loader, to_orig_fn,cnn,knn):
+    # switch to evaluate mode
+    self.model.eval()
+    total_time=0
+    total_frames=0
+    # empty the cache to infer in high res
+    if self.gpu:
+      torch.cuda.empty_cache()
+    # initial last_pose matrix
+    last_pose = np.eye(4)
+    with torch.no_grad():
+      end = time.time()
+      for i, (proj_in, proj_mask, _, _, path_seq, path_name, p_x, p_y, proj_range, unproj_range, _, _, _, _, npoints,_, _,) in tqdm(enumerate(loader)):
+        # first cut to rela size (batch size one allows it)
+        p_x = p_x[0, :npoints]
+        p_y = p_y[0, :npoints]
+        proj_range = proj_range[0, :npoints]
+        unproj_range = unproj_range[0, :npoints]
+        path_seq = path_seq[0]
+        path_name = path_name[0]
+        
+        if self.gpu:
+          proj_in = proj_in.cuda()
+          p_x = p_x.cuda()
+          p_y = p_y.cuda()
+          if self.post:
+            proj_range = proj_range.cuda()
+            unproj_range = unproj_range.cuda()
+
+        #compute output
+        _, tran, rot = self.model(proj_in)
+        
+        # save pose
+        relative_matrix = get_transformation_matrix_quaternion(tran, rot)
+        
+        pose_path = os.path.join(self.logdir, "sequences",
+                            path_seq, "poses.txt")
+        
+        # write pose and update last_pose
+        last_pose = write_poses(pose_path, np.dot(self.T_cam_velo,np.dot(relative_matrix,self.T_velo_cam)), last_pose)
+
+
+if __name__ == '__main__': 
+    splits = ["train", "valid", "test"]
+    parser = argparse.ArgumentParser(description='Infer')
+    parser.add_argument(
+        '--dataset', '-d',
+        type=str,
+        required=True,
+        help='Dataset to train with. No Default',)
+    parser.add_argument(
+        '--arch_cfg', '-ac',
+        type=str,
+        required=True,
+        help='Architecture yaml cfg file. See /config/arch for sample. No default!',)
+    parser.add_argument(
+        '--data_cfg', '-dc',
+        type=str,
+        required=False,
+        default='config/labels/semantic-kitti-mos.yaml',
+        help='Classification yaml cfg file. See /config/labels for sample. No default!',)
+    parser.add_argument(
+        '--log', '-l',
+        type=str,
+        default=None,
+        help='Directory to output the infer data. Default: ~/output')
+    parser.add_argument(
+        '--model', '-m',
+        type=str,
+        required=True,
+        default=None,
+        help='Directory to get the model!')  
+    parser.add_argument(
+        '--split', '-s',
+        type=str,
+        required=False,
+        default='valid',
+        help='Split to evaluate on. One of ' +
+             str(splits) + '. Defaults to %(default)s',)
+    FLAGS, unparsed = parser.parse_known_args()
+    if FLAGS.log is None :
+        FLAGS.log = 'output/' + datetime.datetime.now().strftime("%Y-%-m-%d-%H:%M")
+         
+    # open arch config file
+    try:
+        print("Opening arch config file %s" % FLAGS.arch_cfg)
+        ARCH = yaml.safe_load(open(FLAGS.arch_cfg, 'r'))
+    except Exception as e:
+        print(e)
+        print("Error opening arch yaml file.")
+        quit()
+
+    # open data config file
+    try:
+        print("Opening data config file %s" % FLAGS.data_cfg)
+        DATA = yaml.safe_load(open(FLAGS.data_cfg, 'r'))
+    except Exception as e:
+        print(e)
+        print("Error opening data yaml file.")
+        quit()
+    
+    # print summary of what we will do
+    print("----------")
+    print("dataset", FLAGS.dataset)
+    print("arch_cfg", FLAGS.arch_cfg)
+    print("data_cfg", FLAGS.data_cfg)
+    print("log", FLAGS.log)
+    print("model", FLAGS.model)
+    print("----------\n")
+    
+    # create log folder
+    try:
+        if os.path.isdir(FLAGS.log):
+            shutil.rmtree(FLAGS.log)
+        os.makedirs(FLAGS.log)
+        os.makedirs(os.path.join(FLAGS.log, "sequences"))
+        for seq in DATA["split"]["train"]:
+            seq = '{0:02d}'.format(int(seq))
+            print("train", seq)
+            os.makedirs(os.path.join(FLAGS.log, "sequences", seq))
+            os.makedirs(os.path.join(FLAGS.log, "sequences", seq, "predictions"))
+        for seq in DATA["split"]["valid"]:
+            seq = '{0:02d}'.format(int(seq))
+            print("valid", seq)
+            os.makedirs(os.path.join(FLAGS.log, "sequences", seq))
+            os.makedirs(os.path.join(FLAGS.log, "sequences", seq, "predictions"))
+        for seq in DATA["split"]["test"]:
+            seq = '{0:02d}'.format(int(seq))
+            print("test", seq)
+            os.makedirs(os.path.join(FLAGS.log, "sequences", seq))
+            os.makedirs(os.path.join(FLAGS.log, "sequences", seq, "predictions"))
+    except Exception as e:
+        print(e)
+        print("Error creating log directory. Check permissions!")
+        raise
+    
+    # create user and infer dataset
+    user = User(ARCH, DATA, FLAGS.dataset, FLAGS.log, FLAGS.model,FLAGS.split)
+    user.infer()
+    
+    
---
+++ train_motion_localtest.py
@@ -0,0 +1,413 @@
+#!/usr/bin/env python3
+# This file is covered by the LICENSE file in the root of this project.
+
+import argparse
+import os
+import time
+import yaml
+import datetime
+import copy
+
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+from tensorboardX import SummaryWriter as Logger
+
+from utility.dataset.kitti.parser_multiscan import Parser
+from utility.ioueval import iouEval
+from modules.model_motion import MotionNet
+from utility.warmupLR import *
+
+
+parser = argparse.ArgumentParser(description='Training')
+parser.add_argument(
+    '--dataset', '-d',
+    type=str,
+    required=True,
+    help='Dataset to train with. No Default',)
+parser.add_argument(
+    '--arch_cfg', '-ac',
+    type=str,
+    required=True,
+    help='Architecture yaml cfg file. See /config/arch for sample. No default!',)
+parser.add_argument(
+    '--data_cfg', '-dc',
+    type=str,
+    required=False,
+    default='config/labels/semantic-kitti-mos.yaml',
+    help='Classification yaml cfg file. See /config/labels for sample. No default!',)
+parser.add_argument(
+    '--log', '-l',
+    type=str,
+    default=None,
+    help='Directory to put the log data. Default: ~/logs/date+time')
+parser.add_argument(
+    '--pretrained', '-p',
+    type=str,
+    required=False,
+    default=None,
+    help='Directory to get the pretrained model. If not passed, do from scratch!')
+
+def main():
+    FLAGS, unparsed = parser.parse_known_args()
+    if FLAGS.log is None and FLAGS.pretrained is not None:
+        FLAGS.log = FLAGS.pretrained
+    if FLAGS.log is None and FLAGS.pretrained is None:
+        FLAGS.log = 'output/logs/' + datetime.datetime.now().strftime("%Y-%-m-%d-%H:%M")
+         
+    # open arch config file
+    try:
+        print("Opening arch config file %s" % FLAGS.arch_cfg)
+        ARCH = yaml.safe_load(open(FLAGS.arch_cfg, 'r'))
+    except Exception as e:
+        print(e)
+        print("Error opening arch yaml file.")
+        quit()
+
+    # open data config file
+    try:
+        print("Opening data config file %s" % FLAGS.data_cfg)
+        DATA = yaml.safe_load(open(FLAGS.data_cfg, 'r'))
+    except Exception as e:
+        print(e)
+        print("Error opening data yaml file.")
+        quit()
+    
+    # print summary of what we will do
+    print("----------")
+    print("dataset", FLAGS.dataset)
+    print("arch_cfg", FLAGS.arch_cfg)
+    print("data_cfg", FLAGS.data_cfg)
+    print("log", FLAGS.log)
+    print("pretrained", FLAGS.pretrained)
+    print("----------\n")
+    
+    # start training
+    main_worker(FLAGS)
+    
+def main_worker(args):
+    # open arch config file
+    try:
+        print("Opening arch config file %s" % args.arch_cfg)
+        ARCH = yaml.safe_load(open(args.arch_cfg, 'r'))
+    except Exception as e:
+        print(e)
+        print("Error opening data yaml file.")
+        quit()
+    # open data config file
+    try:
+        print("Opening data config file %s" % args.data_cfg)
+        DATA = yaml.safe_load(open(args.data_cfg, 'r'))
+    except Exception as e:
+        print(e)
+        print("Error opening data yaml file.")
+        quit()
+     
+    start_epoch = 0
+    max_epoch = ARCH["train"]["max_epochs"]
+    tb_logger = Logger(args.log + "/tb")
+    device = 'cuda'
+    
+    # Data loading code
+    parser = Parser(root=args.dataset,
+                    train_sequences=DATA["split"]["train"],
+                    valid_sequences=DATA["split"]["valid"],
+                    test_sequences=None,
+                    split='train',
+                    labels=DATA["labels"],
+                    color_map=DATA["color_map"],
+                    learning_map=DATA["learning_map"],
+                    learning_map_inv=DATA["learning_map_inv"],
+                    sensor=ARCH["dataset"]["sensor"],
+                    max_points=ARCH["dataset"]["max_points"],
+                    batch_size=ARCH["train"]["batch_size"],
+                    workers=ARCH["train"]["workers"],
+                    gt=True,
+                    shuffle_train=True)
+    train_loader = parser.get_train_set()
+    valid_loader = parser.get_valid_set()
+    
+    # weights for loss (and bias)
+    epsilon_w = ARCH["train"]["epsilon_w"]
+    content = torch.zeros(parser.get_n_classes(), dtype=torch.float)
+    for cl, freq in DATA["content"].items():
+        x_cl = parser.to_xentropy(cl)  # map actual class to xentropy class
+        content[x_cl] += freq
+    loss_w = 1 / (content + epsilon_w)  # get weights
+    for x_cl, w in enumerate(loss_w):  # ignore the ones necessary to ignore
+        if DATA["learning_ignore"][x_cl]:
+            # don't weigh
+            loss_w[x_cl] = 0
+    print("Loss weights from content: ", loss_w.data)
+    
+    ignore_class = []
+    for i, w in enumerate(loss_w):
+        if w < 1e-10:
+            ignore_class.append(i)
+            print("Ignoring class ", i, " in IoU evaluation")
+    
+    # create model
+    print("=> creating model '{}'".format(args.arch_cfg))
+    with torch.no_grad():
+        model = MotionNet(input_scan=ARCH["train"]["n_input_scans"],
+                       motion_backbone=ARCH["train"]["motion_backbone"])
+    if torch.cuda.is_available():
+        model.cuda()
+        cudnn.benchmark = True
+        cudnn.fastest = True
+    if ARCH["train"]["optimizer"] == "SDG":
+        optimizer = torch.optim.SGD([{'params': model.parameters()}],
+                                    lr=ARCH["train"]["lr"],
+                                    momentum=ARCH["train"]["momentum"],
+                                    weight_decay=ARCH["train"]["w_decay"])
+    elif ARCH["train"]["optimizer"] == "Adam":
+        optimizer = torch.optim.Adam([{'params': model.parameters()}],
+                                    lr=ARCH["train"]["lr"],
+                                    weight_decay=ARCH["train"]["w_decay"])
+    else:
+        raise NotImplementedError
+    
+    # Use warmup learning rate
+    # post decay and step sizes come in epochs and we want it in steps
+    steps_per_epoch = parser.get_train_size()
+    up_steps = int(ARCH["train"]["wup_epochs"] * steps_per_epoch)
+    final_decay = ARCH["train"]["lr_decay"] ** (1 / steps_per_epoch)
+    scheduler = warmupLR(optimizer=optimizer,
+                        lr=ARCH["train"]["lr"],
+                        warmup_steps=up_steps,
+                        momentum=ARCH["train"]["momentum"],
+                        decay=final_decay)
+    
+    # optionally resume from a checkpoint
+    if args.pretrained is not None:
+        model_path = args.pretrained + "/Motion.pth.tar"
+        if os.path.isfile(model_path):
+            print("=> loading checkpoint '{}'".format(model_path))
+            checkpoint = torch.load(model_path)
+            start_epoch = checkpoint['epoch'] + 1
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            scheduler.load_state_dict(checkpoint['scheduler'])
+            print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(model_path, checkpoint['epoch']))
+        else:
+            print("=> no checkpoint found in '{}'".format(args.pretrained))
+    
+    # set train and valid evaluator
+    evaluator = iouEval(parser.get_n_classes(), device, ignore_class)
+    
+    cudnn.benchmark = True
+    
+    # save train info
+    best_train_iou = 0
+    best_valid_iou = 0
+    for epoch in range(start_epoch, max_epoch):
+        # train for one epoch
+        train_iou = train_epoch(train_loader, model, optimizer, evaluator, scheduler, epoch, max_epoch, tb_logger)
+        save_model = copy.deepcopy(model)
+        save_model.my_weak_method = None
+        # checkpoint save
+        if train_iou > best_train_iou:
+            best_train_iou = train_iou
+            save_checkpoint({
+                'epoch': epoch + 1,
+                'arch': 'motion',
+                'state_dict': save_model.state_dict(),
+                # 'optimizer' : optimizer.state_dict(),
+                # 'scheduler': scheduler.state_dict()
+            }, args.log, suffix='_train_best')
+        else:
+            save_checkpoint({
+                'epoch': epoch + 1,
+                'arch': 'motion',
+                'state_dict': save_model.state_dict(),
+                # 'optimizer' : optimizer.state_dict(),
+                # 'scheduler': scheduler.state_dict()
+            }, args.log, suffix='')
+            
+        if epoch % ARCH["train"]["report_epoch"] == 0:
+            # evaluate on validation set
+            print("*" * 70)
+            valid_iou = validate(valid_loader, model, evaluator, parser.get_xentropy_class_string, epoch, tb_logger)
+            if valid_iou > best_valid_iou:
+                best_valid_iou = valid_iou
+                save_checkpoint({
+                    'epoch': epoch + 1,
+                    'arch': 'motion',
+                    'state_dict': save_model.state_dict(),
+                    # 'optimizer' : optimizer.state_dict(),
+                    # 'scheduler': scheduler.state_dict()
+                }, args.log, suffix='_valid_best')
+    print("*" * 80)
+    print('Finished Training')
+        
+def train_epoch(train_loader, model, optimizer, evaluator, scheduler,epoch, max_epoch, logger):
+    batch_time = AverageMeter('Time', ':6.3f')
+    data_time = AverageMeter('Data', ':6.3f')
+    losses = AverageMeter('Losses', ':.4f')
+    losses_tran = AverageMeter('Loss_tran', ':.4f')
+    losses_rot = AverageMeter('Loss_rot', ':.4f')
+    progress = ProgressMeter(
+        len(train_loader),
+        [batch_time, losses],
+        prefix="Epoch: [{}]".format(epoch))
+
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+    for i, (in_vol, _, proj_labels, _, _, _, _, _, _, _, _, _, _, _, _,tran_list, rot_list) in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+        in_vol = in_vol.cuda()
+        tran_labels = tran_list[-1].cuda().float()
+        rot_labels = rot_list[-1].cuda().float()
+        
+        # compute output and loss
+        loss, tran, rot = model(in_vol, tran_labels, rot_labels)
+        loss_sum = loss['sum']
+        loss_tran = loss['tran']
+        loss_rot = loss['rot']
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss_sum.backward()
+        optimizer.step()
+        
+        losses.update(loss_sum.item(), in_vol.size(0))
+        losses_tran.update(loss_tran.item(), in_vol.size(0))
+        losses_rot.update(loss_rot.item(), in_vol.size(0))
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % 40 == 0:
+            progress.display(i)
+            print('train time left: ',calculate_estimate(max_epoch,epoch,i,len(train_loader),data_time.avg, batch_time.avg))
+            
+    # step scheduler
+    scheduler.step()
+            
+    # tensorboard logger
+    logger.add_scalar('train_loss_sum', losses.avg, epoch)
+    logger.add_scalar('train_loss_tran', losses_tran.avg, epoch)
+    logger.add_scalar('train_loss_rot', losses_rot.avg, epoch)
+
+    return losses.avg
+
+    
+def validate(val_loader, model, evaluator, class_func, epoch, logger):
+    batch_time = AverageMeter('Time', ':6.3f')
+    data_time = AverageMeter('Data', ':6.3f')
+    losses = AverageMeter('Losses', ':.4f')
+    losses_tran = AverageMeter('Loss_tran', ':.4f')
+    losses_rot = AverageMeter('Loss_rot', ':.4f')
+
+    # switch to evaluate mode
+    model.eval()
+    evaluator.reset()
+    # empty the cache to infer in high res
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
+        
+    with torch.no_grad():
+        end = time.time()
+        for i, (in_vol, _, proj_labels, _, _, _, _, _, _, _, _, _, _, _, _,tran_list, rot_list) in enumerate(val_loader):
+            # measure data loading time
+            data_time.update(time.time() - end)
+            in_vol = in_vol.cuda()
+            tran_labels = tran_list[-1].cuda().float()
+            rot_labels = rot_list[-1].cuda().float()
+            
+            # compute output and loss
+            loss, tran, rot = model(in_vol, tran_labels, rot_labels)
+            loss_sum = loss['sum']
+            loss_tran = loss['tran']
+            loss_rot = loss['rot']
+            
+            losses.update(loss_sum.item(), in_vol.size(0))
+            losses_tran.update(loss_tran.item(), in_vol.size(0))
+            losses_rot.update(loss_rot.item(), in_vol.size(0))
+            
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+        
+    print('Validation set:\n'
+                'Time avg per batch {batch_time.avg:.3f}\n'
+                'Loss avg {loss.avg:.4f}\n'
+                .format(batch_time =batch_time,
+                                    loss=losses,
+                                    ))    
+       
+    # tensorboard logger
+    logger.add_scalar('valid_loss_sum', losses.avg, epoch)
+    logger.add_scalar('valid_loss_tran', losses_tran.avg, epoch)
+    logger.add_scalar('valid_loss_rot', losses_rot.avg, epoch)
+    
+    return losses.avg
+    
+
+def calculate_estimate(max_epoch, epoch, iter, len_data, data_time_t, batch_time_t):
+        estimate = int((data_time_t + batch_time_t) * (len_data * max_epoch - (iter + 1 + epoch * len_data)))
+        return str(datetime.timedelta(seconds=estimate))
+
+def save_checkpoint(state, logdir, suffix=""):
+    # Save the weights
+    torch.save(state, logdir +
+               "/Mos_odom" + suffix + '.pth.tar')
+
+def save_to_log(logdir, logfile, message):
+    f = open(logdir + '/' + logfile, "a")
+    f.write(message + '\n')
+    f.close()
+    return
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+        
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
+if __name__ == '__main__':   
+    main()
+
+    
